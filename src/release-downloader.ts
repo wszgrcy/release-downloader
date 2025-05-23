bfcc618f1ea1aa362f538a5b7d9f22f4ae5cb456
chen <wszgrcy@gmail.com>
2025-05-23 14:23:42 +0800

    1

--- src/release-downloader.ts
+++ src/release-downloader.ts
@@ -225,7 +225,7 @@ export class ReleaseDownloader {
 
           const dData: DownloadMetaData = {
             fileName: asset.name,
-            url: asset['url'],
+            url: asset['url']??(asset as any)['browser_download_url'],
             isTarBallOrZipBall: false
           }
           downloads.push(dData)
